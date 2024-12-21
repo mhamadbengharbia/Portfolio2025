76e438f665c7abc3ba9809432afaf16176a82ad7
mhamed ben gharbia <mhamedbengharbia@gmail.com>
2024-12-21 11:43:45 +0100

    second commit

---
+++ src/Components/js/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+function Hero() {
+  return (
+    <div className="hero">
+      <h2>Who I Am</h2>
+      <p>Share your story, background, and passion for technology.</p>
+    </div>
+  );
+}
+
+export default Hero;
