3ecce32b39f69200c10238db96259234e31e0f5e
mhamed ben gharbia <mhamedbengharbia@gmail.com>
2024-12-21 13:29:24 +0100

    add hero pic

--- src/Components/js/Hero.js
+++ src/Components/js/Hero.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './style.css';
-
+import MyPic from '../src/Mypic.webp'
 function Hero() {
   return (
     <div className="hero">
-      <img src=""></img>
-
-      <h1>Systems and Network Technician</h1>
+      <img src={MyPic}></img>
+      <h1>Mhamed Ben Gharbia</h1>
+      <h2>Systems and Network Technician</h2>
       <button>More Info</button>
 
     </div>
